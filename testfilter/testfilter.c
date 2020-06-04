55921d9bc222e074837c4771b8e4a1505f376d13
Shubham Dubey <sdubey504@gmail.com>
2020-06-04 02:43:18 -0700

    initial push

---
+++ testfilter/testfilter.c
@@ -0,0 +1,414 @@
+/*++
+
+Module Name:
+
+    testfilter.c
+
+Abstract:
+
+    This is the main module of the testfilter miniFilter driver.
+
+Environment:
+
+    Kernel mode
+
+--*/
+
+#include <fltKernel.h>
+#include <dontuse.h>
+
+#pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
+
+
+PFLT_FILTER gFilterHandle = NULL;
+ULONG_PTR OperationStatusCtx = 1;
+
+#define PTDBG_TRACE_ROUTINES            0x00000001
+#define PTDBG_TRACE_OPERATION_STATUS    0x00000002
+
+ULONG gTraceFlags = 0;
+
+
+#define PT_DBG_PRINT( _dbgLevel, _string )          \
+    (FlagOn(gTraceFlags,(_dbgLevel)) ?              \
+        DbgPrint _string :                          \
+        ((int)0))
+
+/*************************************************************************
+    Prototypes
+*************************************************************************/
+
+EXTERN_C_START
+
+DRIVER_INITIALIZE DriverEntry;
+NTSTATUS
+DriverEntry (
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+    );
+
+NTSTATUS
+testfilterInstanceSetup (
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_ FLT_INSTANCE_SETUP_FLAGS Flags,
+    _In_ DEVICE_TYPE VolumeDeviceType,
+    _In_ FLT_FILESYSTEM_TYPE VolumeFilesystemType
+    );
+
+VOID
+testfilterInstanceTeardownStart (
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_ FLT_INSTANCE_TEARDOWN_FLAGS Flags
+    );
+
+VOID
+testfilterInstanceTeardownComplete (
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_ FLT_INSTANCE_TEARDOWN_FLAGS Flags
+    );
+
+NTSTATUS
+testfilterUnload (
+    _In_ FLT_FILTER_UNLOAD_FLAGS Flags
+    );
+
+NTSTATUS
+testfilterInstanceQueryTeardown (
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_ FLT_INSTANCE_QUERY_TEARDOWN_FLAGS Flags
+    );
+
+FLT_PREOP_CALLBACK_STATUS
+testfilterPreOperation (
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID *CompletionContext
+    );
+
+VOID
+testfilterOperationStatusCallback (
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_ PFLT_IO_PARAMETER_BLOCK ParameterSnapshot,
+    _In_ NTSTATUS OperationStatus,
+    _In_ PVOID RequesterContext
+    );
+
+FLT_POSTOP_CALLBACK_STATUS
+testfilterPostOperation (
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_opt_ PVOID CompletionContext,
+    _In_ FLT_POST_OPERATION_FLAGS Flags
+    );
+
+FLT_PREOP_CALLBACK_STATUS
+testfilterFSPreOperation(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID* CompletionContext
+);
+
+FLT_POSTOP_CALLBACK_STATUS
+testfilterFSPostOperation(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_opt_ PVOID CompletionContext,
+    _In_ FLT_POST_OPERATION_FLAGS Flags
+);
+
+FLT_PREOP_CALLBACK_STATUS
+testfilterPreOperationNoPostOperation (
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID *CompletionContext
+    );
+
+BOOLEAN
+testfilterDoRequestOperationStatus(
+    _In_ PFLT_CALLBACK_DATA Data
+    );
+
+EXTERN_C_END
+
+//
+//  Assign text sections for each routine.
+//
+
+#ifdef ALLOC_PRAGMA
+#pragma alloc_text(INIT, DriverEntry)
+#pragma alloc_text(PAGE, testfilterUnload)
+#pragma alloc_text(PAGE, testfilterInstanceQueryTeardown)
+#pragma alloc_text(PAGE, testfilterInstanceSetup)
+#pragma alloc_text(PAGE, testfilterInstanceTeardownStart)
+#pragma alloc_text(PAGE, testfilterInstanceTeardownComplete)
+#endif
+
+//
+//  operation registration
+//
+
+CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
+
+
+    { IRP_MJ_FILE_SYSTEM_CONTROL,
+      0,
+      testfilterFSPreOperation,
+      testfilterFSPostOperation },
+
+
+    { IRP_MJ_OPERATION_END }
+};
+
+//
+//  This defines what we want to filter with FltMgr
+//
+
+CONST FLT_REGISTRATION FilterRegistration = {
+
+    sizeof( FLT_REGISTRATION ),         //  Size
+    FLT_REGISTRATION_VERSION,           //  Version
+    0,                                  //  Flags
+
+    NULL,                               //  Context
+    Callbacks,                          //  Operation callbacks
+
+    testfilterUnload,                           //  MiniFilterUnload
+
+    NULL,                    //  InstanceSetup - testfilterInstanceSetup
+    NULL,            //  InstanceQueryTeardown - testfilterInstanceQueryTeardown
+    NULL,            //  InstanceTeardownStart - testfilterInstanceTeardownStart
+    NULL,         //  InstanceTeardownComplete - testfilterInstanceTeardownComplete
+
+    NULL,                               //  GenerateFileName
+    NULL,                               //  GenerateDestinationFileName
+    NULL                                //  NormalizeNameComponent
+
+};
+
+
+/*************************************************************************
+    MiniFilter initialization and unload routines.
+*************************************************************************/
+
+NTSTATUS
+DriverEntry (
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+    )
+/*++
+
+Routine Description:
+
+    This is the initialization routine for this miniFilter driver.  This
+    registers with FltMgr and initializes all global data structures.
+
+Arguments:
+
+    DriverObject - Pointer to driver object created by the system to
+        represent this driver.
+
+    RegistryPath - Unicode string identifying where the parameters for this
+        driver are located in the registry.
+
+Return Value:
+
+    Routine can return non success error codes.
+
+--*/
+{
+    NTSTATUS status;
+
+    UNREFERENCED_PARAMETER( RegistryPath );
+
+    PT_DBG_PRINT( PTDBG_TRACE_ROUTINES,
+                  ("testfilter!DriverEntry: Entered\n") );
+
+    //
+    //  Register with FltMgr to tell it our callback routines
+    //
+
+    status = FltRegisterFilter( DriverObject,
+                                &FilterRegistration,
+                                &gFilterHandle );
+
+    FLT_ASSERT( NT_SUCCESS( status ) );
+
+    if (NT_SUCCESS( status )) {
+
+        //
+        //  Start filtering i/o
+        //
+
+        status = FltStartFiltering( gFilterHandle );
+
+        if (!NT_SUCCESS( status )) {
+
+            FltUnregisterFilter( gFilterHandle );
+        }
+    }
+
+    return status;
+}
+
+NTSTATUS
+testfilterUnload (
+    _In_ FLT_FILTER_UNLOAD_FLAGS Flags
+    )
+/*++
+
+Routine Description:
+
+    This is the unload routine for this miniFilter driver. This is called
+    when the minifilter is about to be unloaded. We can fail this unload
+    request if this is not a mandatory unload indicated by the Flags
+    parameter.
+
+Arguments:
+
+    Flags - Indicating if this is a mandatory unload.
+
+Return Value:
+
+    Returns STATUS_SUCCESS.
+
+--*/
+{
+    UNREFERENCED_PARAMETER( Flags );
+
+    PAGED_CODE();
+
+    PT_DBG_PRINT( PTDBG_TRACE_ROUTINES,
+                  ("testfilter!testfilterUnload: Entered\n") );
+
+    FltUnregisterFilter( gFilterHandle );
+
+    return STATUS_SUCCESS;
+}
+
+
+/*************************************************************************
+    MiniFilter callback routines.
+*************************************************************************/
+
+
+
+FLT_PREOP_CALLBACK_STATUS
+testfilterFSPreOperation(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID* CompletionContext
+)
+{
+    NTSTATUS status;
+
+    UNREFERENCED_PARAMETER(FltObjects);
+    UNREFERENCED_PARAMETER(CompletionContext);
+    PFLT_FILE_NAME_INFORMATION FileNameInfo;
+    WCHAR Name[300] = { 0 };
+    //WCHAR MinorFunc[300] = { 0 };
+    PT_DBG_PRINT(PTDBG_TRACE_ROUTINES,
+        ("testfilter!testfilterPreOperation: Entered\n"));
+
+    status = FltGetFileNameInformation(Data, FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT, &FileNameInfo);
+    //FltObjects->FileObject->FileName;
+    //Data->Iopb->TargetFileObject->FileName
+    //Data->TagData->MountPointReparseBuffer.PathBuffer
+    //Data->Iopb->Parameters.DeviceIoControl.Buffered.IoControlCode
+    if (NT_SUCCESS(status))
+    {
+        status = FltParseFileNameInformation(FileNameInfo);
+
+        if (NT_SUCCESS(status))
+        {
+            // 589988 is for SET_REPARSE_POINT
+            // But also look for 590400
+            KdPrint(("Device Control Code: %lu \r\n", Data->Iopb->Parameters.DeviceIoControl.Buffered.IoControlCode));
+            if (FileNameInfo->Name.MaximumLength < 260)
+            {
+                RtlCopyMemory(Name, Data->Iopb->TargetFileObject->FileName.Buffer, Data->Iopb->TargetFileObject->FileName.Length);
+                
+                KdPrint(("FS IO Name- request happens: %ws \r\n", Name));
+                
+                //KdPrint(("Minor Function: %ws \r\n", Data->Iopb->MajorFunction));
+                KdPrint(("Next line \r\n"));
+                
+            }
+        }
+
+        FltReleaseFileNameInformation(FileNameInfo);
+    }
+
+    // This template code does not do anything with the callbackData, but
+    // rather returns FLT_PREOP_SUCCESS_WITH_CALLBACK.
+    // This passes the request down to the next miniFilter in the chain.
+
+    return FLT_PREOP_SUCCESS_WITH_CALLBACK;
+}
+
+
+FLT_POSTOP_CALLBACK_STATUS
+testfilterFSPostOperation(
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _In_opt_ PVOID CompletionContext,
+    _In_ FLT_POST_OPERATION_FLAGS Flags
+)
+
+
+{
+    UNREFERENCED_PARAMETER(Data);
+    UNREFERENCED_PARAMETER(FltObjects);
+    UNREFERENCED_PARAMETER(CompletionContext);
+    UNREFERENCED_PARAMETER(Flags);
+
+    PT_DBG_PRINT(PTDBG_TRACE_ROUTINES,
+        ("testfilter!testfilterPostOperation: Entered\n"));
+
+    return FLT_POSTOP_FINISHED_PROCESSING;
+}
+
+FLT_PREOP_CALLBACK_STATUS
+testfilterPreOperationNoPostOperation (
+    _Inout_ PFLT_CALLBACK_DATA Data,
+    _In_ PCFLT_RELATED_OBJECTS FltObjects,
+    _Flt_CompletionContext_Outptr_ PVOID *CompletionContext
+    )
+/*++
+
+Routine Description:
+
+    This routine is a pre-operation dispatch routine for this miniFilter.
+
+    This is non-pageable because it could be called on the paging path
+
+Arguments:
+
+    Data - Pointer to the filter callbackData that is passed to us.
+
+    FltObjects - Pointer to the FLT_RELATED_OBJECTS data structure containing
+        opaque handles to this filter, instance, its associated volume and
+        file object.
+
+    CompletionContext - The context for the completion routine for this
+        operation.
+
+Return Value:
+
+    The return value is the status of the operation.
+
+--*/
+{
+    UNREFERENCED_PARAMETER( Data );
+    UNREFERENCED_PARAMETER( FltObjects );
+    UNREFERENCED_PARAMETER( CompletionContext );
+
+    PT_DBG_PRINT( PTDBG_TRACE_ROUTINES,
+                  ("testfilter!testfilterPreOperationNoPostOperation: Entered\n") );
+
+    // This template code does not do anything with the callbackData, but
+    // rather returns FLT_PREOP_SUCCESS_NO_CALLBACK.
+    // This passes the request down to the next miniFilter in the chain.
+
+    return FLT_PREOP_SUCCESS_NO_CALLBACK;
+}
+
